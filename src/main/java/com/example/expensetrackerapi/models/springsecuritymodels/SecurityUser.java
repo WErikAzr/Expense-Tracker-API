3cbfd2f5f0d5d341505f3c9a175b25a0830903ed
erikazr <erikazael47@gmail.com>
2026-08-17 20:48:08 -0600

    Adding CrudRepository and a wrapper for User using SecurityUser

---
+++ src/main/java/com/example/expensetrackerapi/models/springsecuritymodels/SecurityUser.java
@@ -0,0 +1,35 @@
+package com.example.expensetrackerapi.models.springsecuritymodels;
+
+import com.example.expensetrackerapi.models.User;
+import org.jspecify.annotations.NonNull;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+public class SecurityUser implements UserDetails {
+
+    private final User user;
+
+    public SecurityUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    @NonNull
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    @NonNull
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
--- src/main/java/com/example/expensetrackerapi/models/springsecuritymodels/UserDetails.java
+++
@@ -1,4 +0,0 @@
-package com.example.expensetrackerapi.models.springsecuritymodels;
-
-public class UserDetails {
-}
